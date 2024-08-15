2156d0627054d6a616b4d1b273fc1f9194c99aeb
Gregory <mr.greg.cherl@gmail.com>
2024-08-15 22:24:26 +0400

    feature: добавлена маршрутизация

--- apps/app/app.tsx
+++ apps/app/app.tsx
@@ -1,7 +1,11 @@
+import { Link, Outlet } from 'react-router-dom'
+
 export const App = () => {
 	return (
 		<>
 			<div>Start</div>
+			<Link to={'/all-heroes'}>all heroes</Link>
+			<Outlet />
 		</>
 	)
 }
--- apps/index.tsx
+++ apps/index.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './app/app'
+import { router } from './router/router'
+import { RouterProvider } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<RouterProvider router={router} />
+)
