8109f1927c90d8ca898a61f1262b88c0019e2934
Gregory <mr.greg.cherl@gmail.com>
2024-09-14 19:37:26 +0400

    feature: добавление reduce для героев

--- src/components/main/Appearance/Appearance.tsx
+++ src/components/main/Appearance/Appearance.tsx
@@ -1,31 +1,43 @@
+import { FC } from 'react'
 import classes from './Appearance.module.css'
 
-export const Appearance = () => {
+type PropsType = {
+	data: any
+}
+
+export const Appearance: FC<PropsType> = ({ data }) => {
 	return (
 		<div className={classes['appearance']}>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>gender</div>
-				<div className={classes['appearance-value']}>Male</div>
+				<div className={classes['appearance-value']}>{data?.['gender']}</div>
 			</div>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>race</div>
-				<div className={classes['appearance-value']}>human</div>
+				<div className={classes['appearance-value']}>{data?.['race']}</div>
 			</div>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>height</div>
-				<div className={classes['appearance-value']}>5' 10''</div>
+				<div className={classes['appearance-value']}>
+					{`${data?.['height'][0]} ${data?.['height'][1]}`}
+				</div>
 			</div>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>weight</div>
-				<div className={classes['appearance-value']}>170 lb</div>
+				<div className={classes['appearance-value']}>
+					{' '}
+					{`${data?.['weight'][0]} ${data?.['weight'][1]}`}
+				</div>
 			</div>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>eye-color</div>
-				<div className={classes['appearance-value']}>blue</div>
+				<div className={classes['appearance-value']}>{data?.['eye-color']}</div>
 			</div>
 			<div className={classes['appearance-item']}>
 				<div className={classes['appearance-label']}>hair-color</div>
-				<div className={classes['appearance-value']}>black</div>
+				<div className={classes['appearance-value']}>
+					{data?.['hair-color']}
+				</div>
 			</div>
 		</div>
 	)
--- src/components/main/Powerstats/Powerstats.tsx
+++ src/components/main/Powerstats/Powerstats.tsx
@@ -1,31 +1,38 @@
+import { FC } from 'react'
 import classes from './Powerstats.module.css'
 
-export const Powerstats = () => {
+type PropsType = {
+	data: any
+}
+
+export const Powerstats: FC<PropsType> = ({ data }) => {
 	return (
 		<div className={classes['powerstats']}>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>intelligence</span>
+				<span className={classes['powerstats-value']}>
+					{data?.intelligence}
+				</span>
 			</div>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>strength</span>
+				<span className={classes['powerstats-value']}>{data?.strength}</span>
 			</div>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>speed</span>
+				<span className={classes['powerstats-value']}>{data?.speed}</span>
 			</div>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>durability</span>
+				<span className={classes['powerstats-value']}>{data?.durability}</span>
 			</div>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>power</span>
+				<span className={classes['powerstats-value']}>{data?.power}</span>
 			</div>
 			<div className={classes['powerstats-item']}>
-				<span className={classes['powerstats-label']}>Интеллект</span>
-				<span className={classes['powerstats-value']}>100</span>
+				<span className={classes['powerstats-label']}>combat</span>
+				<span className={classes['powerstats-value']}>{data?.combat}</span>
 			</div>
 		</div>
 	)
